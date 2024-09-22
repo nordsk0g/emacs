6629415795b731e23ea87b4ea7ef37e6706c0f50
Steven <steven.christian@gmail.com>
2024-09-22 13:55:36 +0200

    Add org-roam, treemacs, and rust-analyzer for LSP

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 ;; Package manager
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 ;; General styling
@@ -78,27 +79,136 @@
 ;; Org mode
 (use-package org)
 
+;; Org-roam
+(use-package org-roam
+  :custom
+  (org-roam-directory "~/org-roam")
+  (org-roam-completion-everywhere t)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+	 ("C-c n f" . org-roam-node-find)
+	 ("C-c n i" . org-roam-node-insert)
+	 ("C-c n I" . org-roam-node-insert-immediate)
+	 :map org-mode-map
+	 ("C-M-i"   . completion-at-point)))
+
+;; Org-roam immediate insert
+(defun org-roam-node-insert-immediate (arg &rest args)
+  (interactive "P")
+  (let ((args (cons arg args))
+	(org-roam-capture-templates (list (append (car org-roam-capture-templates)
+						  '(:immediate-finish t)))))
+  (apply #'org-roam-node-insert args)))
+
 ;; Consult
 (use-package consult)
 
-;; Neotree
-(require 'neotree)
-(global-set-key [f8] 'neotree-toggle)
+;; Treemacs
+(use-package treemacs
+  :ensure t
+  :bind
+  (:map global-map
+	([f8] . treemacs)
+	("C-<f8>" . treemacs-select-window))
+  :config
+  (setq treemacs-is-never-other-window t)
+  (setq treemacs-follow-mode t)
+  (setq treemacs-project-follow-mode t))
+
+(use-package treemacs-evil
+  :after (treemacs evil)
+  :ensure t)
+
+(use-package treemacs-projectile
+  :after (treemacs projectile)
+  :ensure t)
+
+(use-package treemacs-magit
+  :after (treemacs magit)
+  :ensure t)
+
+(use-package treemacs-icons-dired
+  :hook (dired-mode . treemacs-icons-dired-enable-once)
+  :ensure t)
 
 ;; LSP
 (use-package lsp-mode
-  :commands (lsp lsp-deferred)
+  :commands (lsp)
+  :custom
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
   :init
   (setq lsp-keymap-prefix "C-c l")
+  (setq lsp-inlay-hint-enable t)
   :hook (csharp-mode . lsp)
   :hook (c-mode . lsp)
   :hook (go-mode . lsp)
   :config
-  (lsp-enable-which-key-integration t))
+  (lsp-enable-which-key-integration t)
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
 (use-package lsp-ui :commands lsp-ui-mode)
 (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
 (use-package dap-mode)
 (use-package dap-netcore)
+(add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'rust-mode-hook 'lsp-deferred)
+
+(use-package lsp-ui
+  :ensure
+  :commands lsp-ui-mode
+  :custom
+  (lsp-ui-peek-always-show t)
+  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-doc-enable nil))
+
+(use-package company
+  :ensure
+  :custom
+  (company-idle-delay 0.5)
+  :bind
+  (:map company-active-map
+	("C-n" . company-select-next)
+	("C-p" . company-select-previous)
+	("M-<" . company-select-first)
+	("M->" . company-select-last)))
+
+(use-package yasnippet
+  :ensure
+  :config
+  (yas-reload-all)
+  (add-hook 'prog-mode-hook 'yas-minor-mode)
+  (add-hook 'text-mode-hook 'yas-minor-mode))
+
+;; Rust configuration
+(use-package rustic
+  :ensure
+  :bind (:map rustic-mode-map
+              ("M-j" . lsp-ui-imenu)
+              ("M-?" . lsp-find-references)
+              ("C-c C-c l" . flycheck-list-errors)
+              ("C-c C-c a" . lsp-execute-code-action)
+              ("C-c C-c r" . lsp-rename)
+              ("C-c C-c q" . lsp-workspace-restart)
+              ("C-c C-c Q" . lsp-workspace-shutdown)
+              ("C-c C-c s" . lsp-rust-analyzer-status))
+  :config
+  ;; uncomment for less flashiness
+  ;; (setq lsp-eldoc-hook nil)
+  ;; (setq lsp-enable-symbol-highlighting nil)
+  ;; (setq lsp-signature-auto-activate nil)
+
+  ;; comment to disable rustfmt on save
+  (setq rustic-format-on-save t)
+  (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook))
+
+(defun rk/rustic-mode-hook ()
+  ;; so that run C-c C-c C-r works without having to confirm, but don't try to
+  ;; save rust buffers that are not file visiting. Once
+  ;; https://github.com/brotzeit/rustic/issues/253 has been resolved this should
+  ;; no longer be necessary.
+  (when buffer-file-name
+    (setq-local buffer-save-without-query t))
+  (add-hook 'before-save-hook 'lsp-format-buffer nil t))
 
 ;; Which-Key
 (use-package which-key
@@ -114,7 +224,7 @@
   (projectile-mode +1))
 
 ;; Flycheck
-(use-package flycheck)
+(use-package flycheck :ensure)
 (global-flycheck-mode)
 
 (custom-set-variables
@@ -123,7 +233,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(flycheck go-mode rust-mode evil-mu4e sly counsel ivy-rich projectile dap-mode lsp-treemacs lsp-ui which-key lsp-mode neotree consult magit evil-collection doom-themes))
+   '(rustic company yasnippet treemacs-icons-dired treemacs-magit treemacs-projectile treemacs-evil org-roam-ui org-roam flycheck go-mode rust-mode evil-mu4e counsel ivy-rich projectile dap-mode lsp-treemacs lsp-ui which-key lsp-mode consult magit evil-collection doom-themes))
  '(warning-suppress-types '((comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -131,3 +241,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(provide 'init)
+;;; init.el ends here
+
